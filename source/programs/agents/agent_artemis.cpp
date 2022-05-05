e323b121134493963adc6b1fce6d6f9571042aa5
Luke Clements <54648672+lukeclements@users.noreply.github.com>
2022-05-05 00:00:36 +0000

    added takephoto agent request

--- source/programs/agents/agent_artemis.cpp
+++ source/programs/agents/agent_artemis.cpp
@@ -1,31 +1,31 @@
 /********************************************************************
-* Copyright (C) 2015 by Interstel Technologies, Inc.
-*   and Hawaii Space Flight Laboratory.
-*
-* This file is part of the COSMOS/core that is the central
-* module for COSMOS. For more information on COSMOS go to
-* <http://cosmos-project.com>
-*
-* The COSMOS/core software is licenced under the
-* GNU Lesser General Public License (LGPL) version 3 licence.
-*
-* You should have received a copy of the
-* GNU Lesser General Public License
-* If not, go to <http://www.gnu.org/licenses/>
-*
-* COSMOS/core is free software: you can redistribute it and/or
-* modify it under the terms of the GNU Lesser General Public License
-* as published by the Free Software Foundation, either version 3 of
-* the License, or (at your option) any later version.
-*
-* COSMOS/core is distributed in the hope that it will be useful, but
-* WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-* Lesser General Public License for more details.
-*
-* Refer to the "licences" folder for further information on the
-* condititons and terms to use this software.
-********************************************************************/
+ * Copyright (C) 2015 by Interstel Technologies, Inc.
+ *   and Hawaii Space Flight Laboratory.
+ *
+ * This file is part of the COSMOS/core that is the central
+ * module for COSMOS. For more information on COSMOS go to
+ * <http://cosmos-project.com>
+ *
+ * The COSMOS/core software is licenced under the
+ * GNU Lesser General Public License (LGPL) version 3 licence.
+ *
+ * You should have received a copy of the
+ * GNU Lesser General Public License
+ * If not, go to <http://www.gnu.org/licenses/>
+ *
+ * COSMOS/core is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * COSMOS/core is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Refer to the "licences" folder for further information on the
+ * condititons and terms to use this software.
+ ********************************************************************/
 
 // TODO: change include paths so that we can make reference to cosmos using a full path
 // example
@@ -34,6 +34,7 @@
 #include "agent/agentclass.h"
 #include "support/configCosmos.h"
 #include "support/stringlib.h"
+#include "support/datalib.h"
 
 #include <iostream>
 #include <iomanip>
@@ -43,20 +44,22 @@ using namespace std;
 Agent *agent;
 ofstream file;
 
-// Agent requests
-int32_t example_agent_request(string& request, string& response, Agent* agent);
+// Agent requestsmake
+int32_t example_agent_request(string &request, string &response, Agent *agent);
+int32_t request_takephoto(string &request, string &response, Agent *agent);
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-    cout << "Agent Template" << endl;
+    cout << "Agent Artemis" << endl;
     Agent *agent;
     string nodename = "cubesat1";
-    string agentname = "template"; //name of the agent that the request is directed to
+    string agentname = "template"; // name of the agent that the request is directed to
 
     agent = new Agent(nodename, agentname);
 
     // Add agent requests
     agent->add_request("example_req", example_agent_request, "example_req [args...]", "Prints out any number of args you pass to this");
+    agent->add_request("takephoto", request_takephoto, "", "Takes a photo using the Raspi Camera");
 
     // add state of health (soh)
     string soh = "{\"device_batt_current_000\"}";
@@ -68,7 +71,7 @@ int main(int argc, char** argv)
     // SOH for location information (utc, position in ECI, attitude in ICRF)
     soh = "{\"node_loc_utc\","
           "\"node_loc_pos_eci\","
-          "\"node_loc_att_icrf\"}" ;
+          "\"node_loc_att_icrf\"}";
 
     // TODO: create append function for soh to make it easier to add strings
     // example: soh.append("node_loc_pos_eci");
@@ -80,17 +83,16 @@ int main(int argc, char** argv)
     //    agent->set_sohstring(soh);
 
     // Start executing the agent
-    while(agent->running())
+    while (agent->running())
     {
         // Start executing the agent
         pos_eci.utc = currentmjd(0);
         agent->cinfo->node.loc.pos.eci = pos_eci;
-//        agent->cinfo->device[0].imu.temp = 123;
+        //        agent->cinfo->device[0].imu.temp = 123;
 
-        //sleep for 1 sec
+        // sleep for 1 sec
         COSMOS_SLEEP(0.1);
     }
-
 
     return 0;
 }
@@ -100,7 +102,7 @@ int main(int argc, char** argv)
 //! \param response The string shown to the user when the agent request returns
 //! \param agent The agent to operate on
 //! \return 0
-int32_t example_agent_request(string& request, string& response, Agent* agent)
+int32_t example_agent_request(string &request, string &response, Agent *agent)
 {
     // The first element is always the name of the agent request,
     // any additional arguments to the agent request will be elements 1 and beyond
@@ -111,7 +113,7 @@ int32_t example_agent_request(string& request, string& response, Agent* agent)
     cout << "Arguments:" << endl;
     for (size_t i = 0; i < args.size(); ++i)
     {
-        cout << std::setw(4) << to_string(i) << ": "<< args[i] << endl;
+        cout << std::setw(4) << to_string(i) << ": " << args[i] << endl;
     }
     cout << endl;
 
@@ -121,3 +123,14 @@ int32_t example_agent_request(string& request, string& response, Agent* agent)
     return 0;
 }
 
+int32_t request_takephoto(string &request, string &response, Agent *agent)
+{
+    // The first element is always the name of the agent request,
+    // any additional arguments to the agent request will be elements 1 and beyond
+    vector<string> args = string_split(request);
+    string result;
+    data_execute("python3 ~/raspicamera.py", result);
+    response = result;
+
+    return 0;
+}
